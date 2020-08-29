3c900d6d18877b35824ba8fdc39a0bafe5086900
ml-aakash-dev <ml.aakash.dev@gmail.com>
2020-08-29 05:39:24 +0500

    header

---
+++ src/components/body/header/header.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+import "../../../css/body/header/header.css"
+
+class header extends Component {
+    render() {
+        return (
+            <div className="header" id="header">
+                <div className="bgimg">     
+                    <div className="image-wrapper">
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default header
